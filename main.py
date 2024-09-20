01b0cffa4c8d782e0cd3980eb3466b745b17481c
Oshawhat <alannobledalL@gmail.com>
2024-09-20 10:17:22 -0500

    stop using multiprocessing (for now)

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import tts
 import multiprocessing
 
 
-def main():
-        _gpt = gpt()
-
-        response = """The meaning of life is a profound and philosophical question that has been contemplated by thinkers, theologians, and individuals throughout history. Answers vary widely depending on cultural, religious, and personal beliefs." +
+test_message = """The meaning of life is a profound and philosophical question that has been contemplated by thinkers, theologians, and individuals throughout history. Answers vary widely depending on cultural, religious, and personal beliefs." +
 
 1. **Philosophical Perspectives**: Some existentialists argue that life has no inherent meaning, and it is up to each individual to create their own purpose. Others, like utilitarians, suggest that the meaning of life could be found in maximizing happiness and well-being.
 
@@ -16,10 +13,18 @@ def main():
 
 4. **Personal Interpretations**: On an individual level, many people find meaning in relationships, personal achievements, love, creativity, and making a positive impact on others and the world.
 
-Ultimately, the meaning of life may be subjective and can differ dramatically from one person to another. It often involves a combination of personal values, experiences, and beliefs."""#_gpt.ask_msg()
+Ultimately, the meaning of life may be subjective and can differ dramatically from one person to another. It often involves a combination of personal values, experiences, and beliefs."""
+
+def main():
+        _gpt = gpt()
+
+        response = "hello world!" #test_message #_gpt.ask_msg()
         response_list: list = response.split(". ")
-        with multiprocessing.Pool(5) as p:
-                p.map(tts.add_to_stack, enumerate(response_list))
+        
+        #with multiprocessing.Pool(5) as p:
+                #p.map(tts.add_to_stack, enumerate(response_list))
+        for ind_sentence in enumerate(response_list):
+                tts.add_to_stack(ind_sentence)
 
 
 if __name__ == "__main__":
