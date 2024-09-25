60d5a0426791abef3e3d826cd74feb0d67a654d8
Oshawhat <alannobledalL@gmail.com>
2024-09-25 14:23:52 -0500

    Change model to gpt-4o, change system instructions

--- gpt.py
+++ gpt.py
@@ -6,24 +6,29 @@ class gpt:
         key_file = open("key.txt", "r")
         openai.api_key = key_file.read()
 
-        self.messages = [ {"role": "system", "content": 
-                    "You are a intelligent assistant."} ]
-    
+        self.messages = [ {
+                "role": "system",
+                "content": 
+                    """
+                        You are a comedy robot who tell jokes based on the prompt.
+                        Do not start your messages with something like \"Sure\", just go straight into what is asked.
+                        Make sure the jokes are original and as funny as possible.
+                    """
+            } ]
 
     def ask_msg(self) -> str:
 
-        message = input("User : ")
+        message = input("\nWhat kind of joke would you like? : ")
         
-        if message:
-            self.messages.append(
-                {"role": "user", "content": message},
-            )
-            chat = openai.ChatCompletion.create(
-                model="gpt-4o-mini", messages = self.messages
-            )
+        if not message:
+            print("Invalid message!")
+            return self.ask_msg()
+        
+        self.messages.append( {"role": "user", "content": message} )
+        chat = openai.ChatCompletion.create(model="gpt-4o", messages=self.messages)
         
         reply = chat.choices[0].message.content
         
-        self.messages.append({"role": "assistant", "content": reply})
+        self.messages.append( {"role": "assistant", "content": reply} )
 
         return reply
